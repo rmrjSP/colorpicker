79279d3cfae164aa0ace79f601e60865f963fcce
Ramraj <ramrage13@gmail.com>
2021-10-21 08:24:12 -0700

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {
+    color: "#FFFFFF",
+    textColor: "#000000"
+  });
+});
+
+
+router.post('/', function(req,res){
+  res.render('index', {
+    color: req.body.colorPicker,
+    textColor: req.body.textColorPicker
+  })
+});
+router.post('/random', function(req,res){
+  res.render('index', {
+    color: generateRandomHexCode(),
+    textColor: generateRandomHexCode()
+  })
+})
+
+function generateRandomHexCode(){
+  let hexCode = "#"
+  while ( hexCode.length < 7) {
+    hexcode += (Math.round(Math.random() * 15)).toString(16)
+  }
+  return hexCode
+}
+
+
+
+module.exports = router;
